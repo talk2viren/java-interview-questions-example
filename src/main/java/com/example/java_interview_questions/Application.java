00d83e0d6fe89239b184c07a2bdb47a8411c3de9
talk2viren <virendra.yadav@outlook.com>
2025-03-02 23:24:09 -0800

    draft

---
+++ src/main/java/com/example/java_interview_questions/Application.java
@@ -0,0 +1,15 @@
+package com.example.java_interview_questions;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+
+	 // end::myMethod[]
+
+}
